481e527757ccb0ad8f2d840e098781fc3afb7072
draglag <dragolariano@gmail.com>
2017-11-21 22:54:33 +0100

    first commit

---
+++ birra.py
@@ -0,0 +1,17 @@
+import sopel.module
+
+@sopel.module.commands('birra')
+def birra(bot, trigger):
+    bot.say(' .   *   ..  . *  *')
+    bot.say(' *  * @()Ooc()*   o  .  ')
+    bot.say('     (Q@*0CG*O()  ___ ')  
+    bot.say('    |\_________/|/ _ \'') 
+    bot.say('    |  |  |  |  | / | | ')
+    bot.say('    |  |  |  |  | | | | ')
+    bot.say('    |  |  |  |  | | | | ')
+    bot.say('    |  |  |  |  | | | | ')
+    bot.say('    |  |  |  |  | | | | ')
+    bot.say('    |  |  |  |  | \_| | ')
+    bot.say('    |  |  |  |  |\___/  ')
+    bot.say('    |\_|__|__|_/|       ')
+    bot.say('     \_________/        ')
